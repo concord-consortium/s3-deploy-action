b2b0d0d5d187238704301594c2cee8b59aa60b2f
Scott Cytacki <scytacki@concord.org>
2021-04-16 23:14:50 -0400

    run npm run build, pass SUB_DIR_PATH

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import * as core from '@actions/core';
 import {context} from '@actions/github';
+import * as exec from '@actions/exec';
 import {getDeployPath} from './deploy-path'
+import * as process from 'process'
 
 async function run(): Promise<void> {
   try {
     const deployPath = getDeployPath(context.ref);
     core.info(`deployPath: ${deployPath}`);
-
+    process.env['SUB_DIR_PATH'] = deployPath;
+    await exec.exec('npm run build');
     core.setOutput('deployPath', deployPath);
   } catch (error) {
     core.setFailed(error.message)
