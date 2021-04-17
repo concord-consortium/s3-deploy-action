d9a6380329ab55f4d1785ecdceabf2d473205bab
Scott Cytacki <scytacki@concord.org>
2021-04-16 21:41:38 -0400

    this just computes the deploy path from context

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,24 +1,27 @@
-import {wait} from '../src/wait'
+import {getDeployPath} from '../src/deploy-path'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+test('getDeployPath version release', () => {
+  expect(getDeployPath("refs/tags/v1.2.3")).toEqual("version/v1.2.3");
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+test('getDeployPath PT prefix branch', () => {
+  expect(getDeployPath("refs/heads/123456789-test-branch")).toEqual("branch/test-branch");
 })
 
-// shows how the runner will run a javascript action with env / stdout protocol
+test('getDeployPath PT suffix branch', () => {
+  expect(getDeployPath("refs/heads/test-branch-123456789")).toEqual("branch/test-branch");
+})
+
+test('getDeployPath not PT branch', () => {
+  expect(getDeployPath("refs/heads/test-branch")).toEqual("branch/test-branch");
+})
+
+// Test the action using the env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['GITHUB_REF'] = "refs/heads/test-branch";
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import {context} from '@actions/github';
+import {getDeployPath} from './deploy-path'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const deployPath = getDeployPath(context.ref);
+    core.info(`deployPath: ${deployPath}`);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('deployPath', deployPath);
   } catch (error) {
     core.setFailed(error.message)
   }
