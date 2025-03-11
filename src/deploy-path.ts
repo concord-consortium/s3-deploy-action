afb500e72432bd9fbdcc2dbfb57ff55bcce6f58d
Scott Cytacki <scytacki@concord.org>
2025-03-11 09:25:22 -0400

    find correct deploy path on pull_request trigger
    The context.ref of GITHUB_REF will be set to a merge branch when the github action is triggered by a pull_request event.
    The GITHUB_HEAD_REF will be set to the branch that is associated with the pull request.

--- src/deploy-path.ts
+++ src/deploy-path.ts
@@ -1,9 +1,8 @@
 import * as core from "@actions/core";
-import {context} from "@actions/github";
 import {getDeployProps} from "./deploy-props";
 
 try {
-  const {deployPath} = getDeployProps(context.ref);
+  const {deployPath} = getDeployProps();
   core.info(`deployPath: ${deployPath}`);
 
   // Output the deployPath so it can be used by an independent build step
--- src/deploy-props.ts
+++ src/deploy-props.ts
@@ -1,7 +1,30 @@
-export function getDeployProps(gitRefs: string): {deployPath: string, version?: string, branch?: string} {
-  const versionMatch = gitRefs.match(/refs\/tags\/(.*)/);
+
+// We use environment variables to figure out the branch or tag name:
+// https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+// Instead we could use the context provided by @actions/github, but that makes this harder to test
+// and requires more logic to make sure we are looking at the right ref.
+//
+// Perhaps in the future we'll want more information about the event that triggered
+// the action. In that case it might make sense to switch to using the context 
+// object. Testing this will be harder because we'll have to set GITHUB_EVENT_NAME 
+// and GITHUB_EVENT_PATH and write the payload to the file referred to by 
+// GITHUB_EVENT_PATH.
+// 
+// import {context} from "@actions/github";
+// import {WebhookEventMap, WebhookEventName} from "@octokit/webhooks-definitions/schema";
+// type TypedContext = {
+//   [k in WebhookEventName]: { eventName: k; payload: WebhookEventMap[k] };
+// }[WebhookEventName];
+// const typedContext = context as unknown as TypedContext;
+// if (typedContext.eventName === "pull_request") {
+//   ref = typedContext.payload.pull_request.head.ref;
+// }
+
+export function getDeployProps(refOverride?: string): {deployPath: string, version?: string, branch?: string} {
+  const ref = refOverride || process.env.GITHUB_HEAD_REF || process.env.GITHUB_REF;
+  const versionMatch = ref?.match(/refs\/tags\/(.*)/);
   const version = versionMatch && versionMatch[1];
-  const branchMatch = gitRefs.match(/refs\/heads\/(.*)/);
+  const branchMatch = ref?.match(/refs\/heads\/(.*)/);
   const branch = branchMatch && branchMatch[1];
   if (version) {
     return {
@@ -26,5 +49,5 @@ export function getDeployProps(gitRefs: string): {deployPath: string, version?: 
       branch: strippedBranch
     };
   }
-  throw new Error(`Unknown ref: ${gitRefs}`);
+  throw new Error(`Unknown ref: ${ref}`);
 }
